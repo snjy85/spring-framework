e8030398d021b517368ae89998a76575eaa51206
Sanjay Jain <sanjay.jain@mastercard.com>
2018-12-27 00:04:53 +0530

    Dependency Injection Simple and XML Based Spring Example

---
+++ dependency-injection/src/main/java/com/sanjay/sample/spring/bean/factory/Triangle.java
@@ -0,0 +1,8 @@
+package com.sanjay.sample.spring.bean.factory;
+
+public class Triangle implements Shape {
+
+    public void draw() {
+        System.out.println("In Triangle draw method");
+    }
+}
---
+++ dependency-injection/src/main/java/com/sanjay/sample/spring/simple/Triangle.java
@@ -0,0 +1,8 @@
+package com.sanjay.sample.spring.simple;
+
+public class Triangle implements Shape {
+
+    public void draw() {
+        System.out.println("In Triangle draw method");
+    }
+}
